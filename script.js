9eb46fa72ab335f21319ad8bee08d40ed80c8072
sagarchavhan007 <139324296+sagarchavhan007@users.noreply.github.com>
2025-01-19 20:34:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:["Full Stack Web Developer","web Developer","Graphic Designer","Game Developer"],
+    strings:["Home Automation Engineer","Full Stack Web Developer","Graphic Designer","Game Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
