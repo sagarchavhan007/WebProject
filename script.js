087258a116984d0d87e320d0774a016b16cc46f1
sagarchavhan007 <139324296+sagarchavhan007@users.noreply.github.com>
2025-01-19 18:13:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["Full Stack Web Developer","web Developer","Graphic Designer","Game Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
